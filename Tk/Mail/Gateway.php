b8cc58a89a47e6b3fd6b91f8be5afa6dbd47a856
tropotek <info@tropotek.com>
2022-08-11 08:32:30 +1000

    ~Auto: Commit

--- Tk/Mail/Gateway.php
+++ Tk/Mail/Gateway.php
@@ -5,7 +5,7 @@ use Bs\Config;
 use \PHPMailer\PHPMailer\PHPMailer;
 
 /**
- * @author Michael Mifsud <info@tropotek.com>
+ * @author Michael Mifsud <http://www.tropotek.com/>
  * @see http://www.tropotek.com/
  * @license Copyright 2016 Michael Mifsud
  */
